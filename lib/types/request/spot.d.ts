e1bdeed38b3d96eea1b7691edfbe53bf62aaf97a
李英濠 <yoho@liyinghaodeMacBook-Pro.local>
2023-02-20 17:22:56 +0800

    s

---
+++ lib/types/request/spot.d.ts
@@ -0,0 +1,19 @@
+import { OrderTimeInForce } from './shared';
+export type WalletType = 'spot' | 'mix_usdt' | 'mix_usd';
+export interface NewWalletTransfer {
+    fromType: WalletType;
+    toType: WalletType;
+    amount: string;
+    coin: string;
+    clientOid?: string;
+}
+export interface NewSpotOrder {
+    symbol: string;
+    side: 'buy' | 'sell';
+    orderType: 'limit' | 'market';
+    force: OrderTimeInForce;
+    price?: string;
+    quantity: string;
+    clientOrderId?: string;
+}
+export type NewBatchSpotOrder = Omit<NewSpotOrder, 'symbol'>;
